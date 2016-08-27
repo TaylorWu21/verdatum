ca72c43147d98e02f644df76f8eef499b80df4fd
Taylor Wu <Taylorwu21@gmail.com>
2016-08-27 15:28:57 -0600

    added comments

--- app/components/comments/Comment.js
+++ app/components/comments/Comment.js
@@ -1,1 +1,41 @@
 import React from 'react';
+import $ from 'jquery';
+
+class Comment extends React.Component {
+	constructor(props) {
+		super(props);
+		this.deleteComment = this.deleteComment.bind(this);
+	}
+
+	deleteComment() {
+		$.ajax({
+			url: `/comments/${this.props.id}`,
+			type: "DELETE",
+			data: { id: this.props.id }
+		}).done( () => {
+			this.props.deleteComment(this.props._id)
+		})
+	}
+
+	render() {
+		let comments = this.state.comment.map( comment => {
+			return(
+				<div className="row">
+        <div className="col s12 m6">
+          <div className="card blue-grey darken-1">
+            <div className="card-content white-text">
+              <span className="card-title">{comment.admirer}</span>
+              <p>{comment.content}</p>
+            </div>
+            <div className='card-action'>
+            	<button onClick={this.deleteComment} className='btn'>Delete</button>
+            </div>
+          </div>
+        </div>
+      </div>
+			)
+		})
+	}
+}
+
+export default Comment;
--- app/components/comments/Comments.js
+++ app/components/comments/Comments.js
@@ -1,17 +1,43 @@
 import React from 'react';
 import $ from 'jquery';
+import CommentForm from './CommentForm';
+import Comment from './Comment';
 
 class Comments extends React.Component {
 	constructor(props) {
 		super(props);
+		this.addComment = this.addComment.bind(this);
+		this.state = { comments: [] };
+	}
+
+	componentWillMount() {
+		$.ajax({
+			url: '/comments',
+			type: 'GET',
+			dataType: 'JSON',
+			data: { UserId: this.props.userId }
+		}).done(comments => {
+			this.setState({ comments });
+		})
+	}
+
+	addComment(comment) {
+		this.setState({ comments: [...this.state.comments, comment] });
+		debugger
 	}
 
 	render() {
+		let comments = this.state.comments.map(comment => {
+			return(<Comment key={comment._id} {...comment} />)
+		});
 		return(
 			<div>
-				<h1></h1>
+				{ comments }
+				<div className='row'>
+					<CommentForm addComment={this.addComment} UserId={this.props.UserId} />
+				</div>
 			</div>
-		)
+		);
 	}
 }
 
